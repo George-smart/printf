635a61ec0ae0bd6350823798f2b00d554312faef
Savwede Godswill <godswillsavwede@gmail.com>
2022-10-16 15:33:37 +0100

    modified _printf.c

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: the input string
+ * Return: On success: total count of the characters printed
+ * On failure: -1
+ */
+int _printf(const char *format, ...)
+{
+	int printed_chars;
+	typ funcs[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"%", print_percent},
+		{"d", print_int},
+		{"i", print_int},
+		{"b", print_bin},
+		{"r", print_rev},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	va_start(arg_list, format);
+
+/*This is where the logic is to be implemented*/
+
+
+	va_end(arg_list);
+	return (printed_chars);
+}
