eae9372d46d40611f3bb1b5cd604534a0cba9847
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-18 20:31:39 +0100

    Update string_n_char.c

--- string_n_char.c
+++ string_n_char.c
@@ -11,7 +11,7 @@ int print_char(va_list val)
 {
 	char c;
 
-	c = va_arg(val, int);
+	c = (va_arg(val, int));
 	_putchar(c);
 	return (1);
 }
@@ -27,6 +27,7 @@ int print_str(va_list val)
 {
 	int cnt, i;
 	char *str;
+	
 	str = (va_arg(val char *));
 	if (str == NULL)
 		str = "(null)";
