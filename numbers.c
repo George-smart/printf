810c473995e9458e47b19ace47ceeee45b755d4d
Savwede <111609631+Savwede@users.noreply.github.com>
2022-10-18 23:36:56 +0100

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -1,33 +1,6 @@
 #include "main.h"
 
 
-/**
- * print_number - Prints any integer with putchar
- * @n: Number to prints
- *
- * Return: Nothing
- */
-int print_number(int n)
-{
-	unsigned int x, cnt = 0;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-		cnt += 1;
-	}
-
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
-	cnt += 1;
-
-	return (cnt);
-}
 
 /**
  * print_int - Receives the main string and all the necessary parameters to
@@ -38,10 +11,34 @@ int print_number(int n)
  */
 int print_int(va_list val)
 {
-	int cnt, num = va_arg(val, int);
+	int n;
+	int div;
+	int len;
+	unsigned int num;
 
-	cnt = print_number(num);
-	return (cnt);
+	n  = va_arg(val, int);
+	div = 1;
+	len = 0;
+
+	if (n < 0)
+	{
+		len += _putchar('-');
+		num = n * -1;
+	}
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len += _putchar('0' + num / div);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
 
 /**
