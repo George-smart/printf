a04ca3776ec6750ee956b5f629ab2fe04874f299
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-21 14:44:27 +0100

    Update Bin_Oct_Hex.c

--- Bin_Oct_Hex.c
+++ Bin_Oct_Hex.c
@@ -18,22 +18,24 @@ int print_number(unsigned int n, unsigned int div)
 	return (cnt);
 }
 
+
 /**
- * base_len - Calculates the length for an octal number
- * @num: The number for which the length is being calculated
- * @base: Base to be calculated by
- * Return: An integer representing the length of a number
+ * _memcpy - copy memory area
+ * @dest: Destination for copying
+ * @src: Source to copy from
+ * @n: The number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
  */
-unsigned int base_len(unsigned int num, int base)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; num > 0; i++)
-	{
-		num = num / base;
-	}
-	return (i);
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
+
 
 /**
  * rev_string - reverses a string in place
@@ -74,7 +76,7 @@ void write_base(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-		_write_char(str[i]);
+		_putchar(str[i]);
 }
 
 /**
