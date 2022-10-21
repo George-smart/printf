d9440c0c7130bda7874ec6556ee3eb36d6d42d25
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-21 14:40:34 +0100

    Update Bin_Oct_Hex.c

--- Bin_Oct_Hex.c
+++ Bin_Oct_Hex.c
@@ -19,6 +19,83 @@ int print_number(unsigned int n, unsigned int div)
 }
 
 /**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
+	return (i);
+}
+
+/**
+ * rev_string - reverses a string in place
+ *
+ * @s: string to reverse
+ * Return: A pointer to a character
+ */
+char *rev_string(char *s)
+{
+	int len;
+	int head;
+	char tmp;
+	char *dest;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	_memcpy(dest, s, len);
+	for (head = 0; head < len; head++, len--)
+	{
+		tmp = dest[len - 1];
+		dest[len - 1] = dest[head];
+		dest[head] = tmp;
+	}
+	return (dest);
+}
+
+/**
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
+ */
+void write_base(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
+}
+
+/**
+ * len_of_base - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+unsigned int len_of_base(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
+	return (i);
+}
+
+
+/**
  * print_bin - Receives the main string and all the necessary parameters to
  * print a formated string
  * @val: the input argument
@@ -28,11 +105,37 @@ int print_number(unsigned int n, unsigned int div)
 
 int print_bin(va_list val)
 {
-	int cnt;
-	unsigned int num = va_arg(val, unsigned int), div = 2;
+	iunsigned int num;
+	int i, len;
+	char *str;
+	char *rev_str;
 
-	cnt = print_number(num, div);
-	return (cnt);
+	num = va_arg(val, unsigned int);
+	if (num == 0)
+		return (_putchar('0'));
+	if (num < 1)
+		return (-1);
+	len = len_of_base(num, 2);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (-1);
+
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			str[i] = '0';
+		else
+			str[i] = '1';
+		num = num / 2;
+	}
+	str[i] = '\0';
+	rev_str = rev_string(str);
+	if (rev_str == NULL)
+		return (-1);
+	write_base(rev_str);
+	free(str);
+	free(rev_str);
+	return (len);
 }
 
 /**
