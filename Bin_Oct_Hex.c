e1e7c56c4da706d8b0955dde5c8b989862b5f48b
George Evaristus <70062291+George-smart@users.noreply.github.com>
2022-10-21 14:46:50 +0100

    Update Bin_Oct_Hex.c

--- Bin_Oct_Hex.c
+++ Bin_Oct_Hex.c
@@ -20,84 +20,6 @@ int print_number(unsigned int n, unsigned int div)
 
 
 /**
- * _memcpy - copy memory area
- * @dest: Destination for copying
- * @src: Source to copy from
- * @n: The number of bytes to copy
- * Return: The _memcpy() function returns a pointer to dest.
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-
-
-/**
- * rev_string - reverses a string in place
- *
- * @s: string to reverse
- * Return: A pointer to a character
- */
-char *rev_string(char *s)
-{
-	int len;
-	int head;
-	char tmp;
-	char *dest;
-
-	for (len = 0; s[len] != '\0'; len++)
-	{}
-
-	dest = malloc(sizeof(char) * len + 1);
-	if (dest == NULL)
-		return (NULL);
-
-	_memcpy(dest, s, len);
-	for (head = 0; head < len; head++, len--)
-	{
-		tmp = dest[len - 1];
-		dest[len - 1] = dest[head];
-		dest[head] = tmp;
-	}
-	return (dest);
-}
-
-/**
- * write_base - sends characters to be written on standard output
- * @str: String to parse
- */
-void write_base(char *str)
-{
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-}
-
-/**
- * len_of_base - Calculates the length for an octal number
- * @num: The number for which the length is being calculated
- * @base: Base to be calculated by
- * Return: An integer representing the length of a number
- */
-unsigned int len_of_base(unsigned int num, int base)
-{
-	unsigned int i;
-
-	for (i = 0; num > 0; i++)
-	{
-		num = num / base;
-	}
-	return (i);
-}
-
-
-/**
  * print_bin - Receives the main string and all the necessary parameters to
  * print a formated string
  * @val: the input argument
@@ -112,32 +34,6 @@ int print_bin(va_list val)
 	char *str;
 	char *rev_str;
 
-	num = va_arg(val, unsigned int);
-	if (num == 0)
-		return (_putchar('0'));
-	if (num < 1)
-		return (-1);
-	len = len_of_base(num, 2);
-	str = malloc(sizeof(char) * len + 1);
-	if (str == NULL)
-		return (-1);
-
-	for (i = 0; num > 0; i++)
-	{
-		if (num % 2 == 0)
-			str[i] = '0';
-		else
-			str[i] = '1';
-		num = num / 2;
-	}
-	str[i] = '\0';
-	rev_str = rev_string(str);
-	if (rev_str == NULL)
-		return (-1);
-	write_base(rev_str);
-	free(str);
-	free(rev_str);
-	return (len);
 }
 
 /**
